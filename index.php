8311de88dff9f41cb6d10d52e33c112f1867fe86
FabienMarti <martiretti.fabien@gmail.com>
2020-06-06 14:27:34 +0200

    e

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ex7</title>
+</head>
+<body>
+    <p>
+        <?php
+            $isOk = true;
+            if($isOk == false){
+                echo('c\'est pas bon !!!');
+            }
+            else{
+                echo('c\'est ok !!');
+            }
+        ?>
+    </p>
+</body>
+</html>
